ff9b2016c792b0b4dd8c680c10609c9e939ee185
Luis Grangeiro <luiscarlos.grangeiro@gmail.com>
2021-05-21 00:29:24 -0300

    :art: Faz algumas correções

--- lib/ex_mon/poke_api/client.ex
+++ lib/ex_mon/poke_api/client.ex
@@ -5,12 +5,12 @@ defmodule ExMon.PokeApi.Client do
     plug Tesla.Middleware.JSON
 
     def get_pokemon(name) do
-        "/pokemon/#{name}"
+        "/pokemon/#{name}" #Não tá reconhecendo o /pokemon/
         |> get()
         |> handle_get()
     end
 
-    defp handle_get({:ok, %Tesla.En{status: 200, body: body}}), do: {:ok, body}
-    defp handle_get({:ok, %Tesla.En{status: 404}}), do: {:error, "Pokemon not found!"}
-    defp handle_get({:error, _reason}}), do: :error
+    defp handle_get({:ok, %Tesla.Env{status: 200, body: body}}), do: {:ok, body}
+    defp handle_get({:ok, %Tesla.Env{status: 404}}), do: {:error, "Pokemon not found!"}
+    defp handle_get({:error, _reason}), do: :error
 end
